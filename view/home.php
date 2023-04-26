f2154b310a4b6ebeabd32d56d39d9d68dc8ff54b
Đặng Đình Thuận <thuan.dd.2229@aptechlearning.edu.vn>
2023-04-26 16:28:04 +0700

    add index

---
+++ view/home.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tràng An Plaza</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css">
+	<style>
+		.head1 {
+			font-size: 40px;
+			color: #009900;
+			font-weight: bold;
+		}
+
+		.head2 {
+			font-size: 17px;
+			margin-left: 10px;
+			margin-bottom: 15px;
+		}
+
+		body {
+			margin: 0 auto;
+			background-position: center;
+			background-size: contain;
+		}
+
+		.menu {
+			position: sticky;
+			top: 0;
+			background-color: #009900;
+			padding: 10px 0px 10px 0px;
+			color: white;
+			margin: 0 auto;
+			overflow: hidden;
+		}
+
+		.menu a {
+			float: left;
+			color: white;
+			text-align: center;
+			padding: 14px 16px;
+			text-decoration: none;
+			font-size: 20px;
+		}
+
+		.menu-log {
+			right: auto;
+			float: right;
+		}
+
+		footer {
+			width: 100%;
+			bottom: 0px;
+			background-color: #000;
+			color: #fff;
+			position: relative;
+			padding-top: 20px;
+			padding-bottom: 50px;
+			text-align: center;
+			font-size: 30px;
+			font-weight: bold;
+		}
+
+		.body_sec {
+			margin-left: 20px;
+		}
+	</style>
+</head>
+
+<body>
+
+	<!-- Header Section -->
+	<header>
+		
+	</header>
+
+	<!-- Menu Navigation Bar -->
+	<nav class="navbar navbar-expand-lg bg-dark navbar-dark  ">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">TRÀNG AN PLAZA</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav align-items-lg-center ms-auto me-lg-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">SẢNH TIỆC</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">TIỆC CƯỚI & KỶ NIỆM</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">TIỆC DOANH NGHIỆP</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#">TIỆC NGOÀI</a>
+                        </li>  
+                        <li>
+                            <a class="nav-link" href="#">GALLERY</a>
+                        </li>   
+                        <li>
+                            <a class="nav-link" href="#">LIÊN HỆ</a>
+                        </li>   
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div id="clear"></div>
+
+	<!-- Body section -->
+
+		<section id="Content">
+			<h3>Content section</h3>
+            
+            <div class="container d-block justify-content-center align-items-center">
+                <div class="row">
+                    <div class="col-3"></div>
+                    <div class="col-3 info">
+                        <h6>TRÀNG AN PLAZA</h6>
+                        <p>Ra mắt vào năm 2019, tọa lạc trên phố Tràng Tiền, Tràng An Plaza sở hữu không gian đa chức năng với diện tích sử dụng lên đến 5000m2, 8 đại sảnh rộng rãi có tổng công suất phục vụ cho hàng ngàn khách. Sự phối hợp cao sang và lãng mạn, ẩm thực độc đáo khiến cho Tràng An Plaza là điểm đến để họa nên những gam màu hạnh phúc, sống động cùng với những sự kiện đáng nhớ! </p>
+                    </div>
+                    <div class="col-3"></div>
+                    <div class="col-3"></div>
+                    <div class="image-wrap">
+                        <img src="images/wedding_1.jpg" alt="" class="custom-img">
+                    </div>
+                </div>
+            </div>
+		</section>
+        <div id="clear"></div>
+        <section>
+            <h1>Next content</h1>
+        </section>
+        <section>
+            <h1>Next content</h1>
+        </section>
+        <section>
+            <h1>Next content</h1>
+        </section>
+
+
+	<!-- Footer Section -->
+	<footer>Footer Section</footer>
+</body>
+</html>
---
+++ view/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tràng An Plaza</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <header></header>
+    
+        
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark  ">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">TRÀNG AN PLAZA</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav align-items-lg-center ms-auto me-lg-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">SẢNH TIỆC</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">TIỆC CƯỚI & KỶ NIỆM</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">TIỆC DOANH NGHIỆP</a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#">TIỆC NGOÀI</a>
+                        </li>  
+                        <li>
+                            <a class="nav-link" href="#">GALLERY</a>
+                        </li>   
+                        <li>
+                            <a class="nav-link" href="#">LIÊN HỆ</a>
+                        </li>   
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    <main>
+        <section class="intro-section">
+            <div class="image-wrap">
+                <img src="images/wedding_1.jpg" alt="" class="custom-img">
+            </div>
+            <div class="container d-block justify-content-center align-items-center">
+                <div class="row">
+                    <div class="col-3"></div>
+                    <div class="col-3 info">
+                        <h6>TRÀNG AN PLAZA</h6>
+                        <p>Ra mắt vào năm 2019, tọa lạc trên phố Tràng Tiền, Tràng An Plaza sở hữu không gian đa chức năng với diện tích sử dụng lên đến 5000m2, 8 đại sảnh rộng rãi có tổng công suất phục vụ cho hàng ngàn khách. Sự phối hợp cao sang và lãng mạn, ẩm thực độc đáo khiến cho Tràng An Plaza là điểm đến để họa nên những gam màu hạnh phúc, sống động cùng với những sự kiện đáng nhớ! </p>
+                    </div>
+                    <div class="col-3"></div>
+                    <div class="col-3"></div>
+                </div>
+            </div>
+                
+        </section>
+
+        <section>
+            <h1>section2</h1>
+            <div class="container d-block">
+                <div class=row>
+                    <div class="col 6">
+                        <img src="images/wedding_2.jpg" alt="">
+                    </div>
+
+                </div>
+                
+            </div>
+        </section>
+    
+    </main>
+
+    
+    
+    
+</body>
+</html>
