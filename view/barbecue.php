29f1d613663c3f6343f913985cc19362b276f1a1
datle0102 <131856577+datle0102@users.noreply.github.com>
2023-05-17 22:51:39 +0700

    Update and rename tiecngoai.php to barbecue.php

---
+++ view/barbecue.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tiệc ngoài</title>
+    <link rel="stylesheet" href="css/barbecue.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+	  <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+</head>
+<body>
+<?php require_once('../view/include/header.php'); ?>
+<div id="clear"></div>
+    <main>  
+      <section id="section_1">
+            <div id="carousel-intro" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="images/barbecue_1.webp" class="blockd w-10"  alt="...">
+                <h1 class="section-title section-title-center"> TIỆC NGOÀI - TRÀNG AN PLAZA TÂN PHÚ</h1>
+                <h2>ĐỒNG HÀNH TRONG</h2>
+                <h2>TỪNG TRẢI NGHIỆM</h2>  
+                <div class="centered">
+                <p>Phối hợp ăn ý với yêu cầu riêng của nhà tổ chức, chúng tôi cung cấp trọn gói tiệc phục vụ tại chỗ (Outside Catering). Việc của bạn là chỉ cần chia sẻ với</p>
+                <p>chúng tôi bất cứ địa điểm nào diễn ra sự kiện để Asiana Plaza mang đến trải nghiệm hài lòng và yên tâm từ cung cách phục vụ chuyên nghiệp đến ẩm</p>
+                <p>thực ấn tượng cho khách tham dự.</p>
+                </div>
+      </section>
+        <div id="clear"></div>
+        <section id="section_2" class="container">
+            <div class="row">
+              <div class="col-4">
+                <div class="image-container">
+                    <img src="images/barbecue_2.webp" alt="Ảnh" class="img-fluid " alt="">
+                  <h1>DỊCH VỤ</h1>
+                  <p>Bạn đưa ra đề bài và Asiana Plaza sẽ là người tìm ra giải pháp, đó là cách chúng tôi chinh phục những bản kể hoạch tổ chức tiệc ngoài từ giản đơn đến phức tạp nhất. Với đội ngũ nhiều năm kinh nghiệm, Asiana Plaza bảo đảm duy trì đẳng cấp thương hiệu hàm chứa trong ẩm thực, phong thái phục vụ, tiết mục chương trình và trang trí đến với không gian đia điểm được bạn chỉ định.</p>
+                </div>     
+              </div>
+              <div class="col-8">
+              <div id="carousel-intro" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carousel-intro" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carousel-intro" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carousel-intro" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="images/barbecue_4.webp" class="d-block w-100"  alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/barbecue_5.webp" class="d-block w-100"  alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/barbecue_6.webp" class="d-block w-100 " alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carousel-intro" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carousel-intro" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                </button>
+                <h1>KHAI TRƯƠNG</h1>
+                <h1>RA MẮT DỰ ÁN</h1>
+                <p>Chúng tôi còn cung cấp giải pháp hoàn thiện cho các dịch vụ đặt tiệc Catering tại chỗ. Cho dù lễ khai trương hay ra mắt dự án diễn ra tại không gian trong nhà hay ngoài trời, hãy chia sẻ với chúng tôi kế hoạch của bạn.</p>
+              </div>
+      </section>
+      <section id="section_3" class="container">
+          <div class="row">
+                <div class="col-8"> 
+                <div id="carousel-intro" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carousel-intro" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carousel-intro" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carousel-intro" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="images/barbecue_4.webp" class="d-block w-100"  alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/barbecue_5.webp" class="d-block w-100"  alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/barbecue_6.webp" class="d-block w-100 " alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carousel-intro" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carousel-intro" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                </button>
+                <h1>LỄ KHỞI CÔNG</h1>
+                <p>Luôn đồng hành cùng với các lễ khởi công, động thổ nhà máy, dự án bất động sản, bất cứ địa điểm nào diễn ra sự kiện của bạn, hãy để cung cách phục vụ chuyên nghiệp, ẩm thực ấn tượng từ Asiana Plaza góp phần với khởi đầu mới thành công</p>
+      </section>       
+      <section id="section_4" class="container">
+          <div class="row">
+                <div class="col-8"> 
+                <div id="carousel-intro" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carousel-intro" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carousel-intro" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carousel-intro" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="images/barbecue_4.webp" class="d-block w-100"  alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/barbecue_5.webp" class="d-block w-100"  alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/barbecue_6.webp" class="d-block w-100 " alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carousel-intro" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carousel-intro" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                </button>
+                <h1>TIỆC NGOÀI TRỜI</h1>
+                <p>Tiệc ngoài trời đa phong cách dành cho bất cứ cá nhân nào như tiệc cưới, lễ báo hỷ, sinh nhật. Hãy biến buổi dạ tiệc ngoài trời mang hơi thở thiên nhiên trong lành, thoáng đãng và tạo nên một dấu ấn riêng cùng trọn gói phục vụ Catering từ Asiana Plaza.</p>
+      </section>
+  </main>
+    <div id="clear"></div>
+    <?php require_once('../view/include/footer.php'); ?>
+  </div>
+</div>
+</body>
+</html>
